7b665fc7c03583fc2e878a4e159dc1c3d2e81467
João Trepichio <trepichio@gmail.com>
2021-07-05 11:28:09 -0300

    connect to broker queue and add config for ENV

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  @Get()
-  getHello(): string {
-    return 'hello';
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    const {
+      BROKER_USER,
+      BROKER_PASSWORD,
+      BROKER_IP,
+      BROKER_VIRTUAL_HOST,
+      BROKER_PORT,
+    } = process.env;
+
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          `amqp://${BROKER_USER}:${BROKER_PASSWORD}@${BROKER_IP}:${BROKER_PORT}/${BROKER_VIRTUAL_HOST}`,
+        ],
+        queue: 'admin-backend',
+      },
+    });
   }
+
 }
