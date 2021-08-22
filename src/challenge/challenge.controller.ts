34ef0763ae721e73b4a3ec1e4da848529f771972
João Trepichio <trepichio@gmail.com>
2021-08-21 23:37:05 -0300

    add GET Challenges

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -12,6 +12,7 @@ import {
   ValidationPipe,
   BadRequestException,
 } from '@nestjs/common';
+import { ChallengeInterface } from './interfaces/challenge.interface';
 import { ClientProxySmartRanking } from 'src/proxyrmq/client-proxy.provider';
 
 @Controller('api/v1/challenges')
@@ -27,5 +28,31 @@ export class ChallengeController {
     this.clientProxySmartRanking.getClientProxyInstance('admin');
 
   private readonly logger = new Logger(ChallengeController.name);
+
+  @Get(':challengeId?')
+  async getChallenges(
+    @Param('challengeId') challengeId: string = '',
+    @Query('playerId') playerId: string = '',
+  ): Promise<ChallengeInterface[]> {
+    if (!challengeId && playerId) {
+      /**
+       * Check if playerId is valid
+       */
+      const player = await this.clientAdminBackend
+        .send('get-players', playerId)
+        .toPromise();
+
+      if (!player) {
+        throw new BadRequestException(`Player with id ${playerId} not found`);
+      }
+    }
+
+    /**
+     * Get Challenges (all or by playerId or by challengeId)
+     */
+    return await this.clientChallenges
+      .send('get-challenges', { playerId, challengeId })
+      .toPromise();
+  }
 
 }
